43dc677c5204e6049b79025eb6d7b7d282028aa8
gtz.jesus <gtz.jesus@outlook.com>
2024-04-01 00:23:54 -0600

    craft component redone + design from figma

--- src/components/performance/Performance.jsx
+++ src/components/performance/Performance.jsx
@@ -1,9 +1,23 @@
 import { useState, useEffect, useRef } from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import LazyLoad from 'react-lazyload';
 import FirstPerformance from './FirstPerformance';
 import SecondPerformance from './SecondPerformance';
 import ThirdPerformance from './ThirdPerformance';
+
+const fadeIn = keyframes`
+  0% { opacity: 0; }
+  100% { opacity: 1; }
+`;
+
+const delay = 5; // Adjust the delay value as needed
+
+const AnimatedComponent = styled.div`
+  opacity: 0;
+  animation: ${fadeIn} 15s forwards;
+  animation-delay: ${({ index }) =>
+    index * delay}s; // Apply delay based on index
+`;
 
 const StyledPerformance = styled.div`
   background: var(--color-black);
@@ -42,9 +56,26 @@ function Performance() {
     <LazyLoad>
       <StyledPerformance>
         <Information>
-          <FirstPerformance ref={(el) => (sectionsRef.current[0] = el)} />
-          <SecondPerformance ref={(el) => (sectionsRef.current[1] = el)} />
-          <ThirdPerformance ref={(el) => (sectionsRef.current[2] = el)} />
+          <AnimatedComponent
+            style={{ opacity: activeSection === 0 ? 1 : 0 }}
+            index={0}
+          >
+            <FirstPerformance ref={(el) => (sectionsRef.current[0] = el)} />
+          </AnimatedComponent>
+
+          <AnimatedComponent
+            style={{ opacity: activeSection === 1 ? 1 : 0 }}
+            index={1}
+          >
+            <SecondPerformance ref={(el) => (sectionsRef.current[1] = el)} />
+          </AnimatedComponent>
+
+          <AnimatedComponent
+            style={{ opacity: activeSection === 2 ? 1 : 0 }}
+            index={2}
+          >
+            <ThirdPerformance ref={(el) => (sectionsRef.current[2] = el)} />
+          </AnimatedComponent>
         </Information>
       </StyledPerformance>
     </LazyLoad>
