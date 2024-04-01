fa2ac50fba8dfdb037186a8352ad2b153df64817
gtz.jesus <gtz.jesus@outlook.com>
2024-04-01 01:39:46 -0600

    Finally adding animations, using Intersection Observer API

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,12 +3,14 @@ import Navigation from '../../components/navigation/Navigation';
 import Achievements from '../../components/achievements/Achievements';
 import Source from '../../components/source/Source';
 import Trial from '../../components/trial/Trial';
-import Performance from '../../components/performance/Performance';
 import Footer from '../../components/footer/Footer';
 import Faqs from '../../components/faqs/Faqs';
 import Craft from '../../components/craft/Craft';
 import Refer from '../../components/refer/Refer';
 import Landing from '../../components/landing/Landing';
+import FirstPerformance from '../../components/performance/FirstPerformance';
+import SecondPerformance from '../../components/performance/SecondPerformance';
+import ThirdPerformance from '../../components/performance/ThirdPerformance';
 
 function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -20,7 +22,10 @@ function Home() {
     setIsModalOpen(false);
   };
 
-  const hiddenRef = useRef(null);
+  const sourceRef = useRef(null);
+  const firstPerformanceRef = useRef(null);
+  const secondPerformanceRef = useRef(null);
+  const thirdPerformanceRef = useRef(null);
 
   useEffect(() => {
     const observer = new IntersectionObserver(
@@ -36,13 +41,31 @@ function Home() {
       { threshold: 0.5 } // Adjust the threshold as needed
     );
 
-    if (hiddenRef.current) {
-      observer.observe(hiddenRef.current);
+    if (sourceRef.current) {
+      observer.observe(sourceRef.current);
+    }
+    if (firstPerformanceRef.current) {
+      observer.observe(firstPerformanceRef.current);
+    }
+    if (secondPerformanceRef.current) {
+      observer.observe(secondPerformanceRef.current);
+    }
+    if (thirdPerformanceRef.current) {
+      observer.observe(thirdPerformanceRef.current);
     }
 
     return () => {
-      if (hiddenRef.current) {
-        observer.unobserve(hiddenRef.current);
+      if (sourceRef.current) {
+        observer.unobserve(sourceRef.current);
+      }
+      if (firstPerformanceRef.current) {
+        observer.unobserve(firstPerformanceRef.current);
+      }
+      if (secondPerformanceRef.current) {
+        observer.unobserve(secondPerformanceRef.current);
+      }
+      if (thirdPerformanceRef.current) {
+        observer.unobserve(thirdPerformanceRef.current);
       }
     };
   }, []);
@@ -55,11 +78,21 @@ function Home() {
         openModal={openModal}
         closeModal={closeModal}
       />
-      <div ref={hiddenRef} className="hidden">
+      <div ref={sourceRef} className="hidden">
         <Source />
       </div>
+
       <div className="responsive-container">
-        <Performance />
+        <div ref={firstPerformanceRef} className="hidden">
+          <FirstPerformance />
+        </div>
+        <div ref={secondPerformanceRef} className="hidden">
+          <SecondPerformance />
+        </div>
+        <div ref={thirdPerformanceRef} className="hidden">
+          <ThirdPerformance />
+        </div>
+
         <Craft />
         <Achievements />
         <Faqs />
