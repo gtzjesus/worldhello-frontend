c5224b9c219d8e1f27c17be11d198a490cf7278c
gtz.jesus <gtz.jesus@outlook.com>
2024-04-04 12:31:00 -0600

    Performance component adjustments in UI

--- src/components/performance/Performance.jsx
+++ src/components/performance/Performance.jsx
@@ -1,58 +1,25 @@
-import React from 'react';
-import styled, { createGlobalStyle } from 'styled-components';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+// ------------------------------
+// File: Performance.jsx
+// ------------------------------
+// Description: React complete Home page
+
 import FirstPerformance from './FirstPerformance';
 import SecondPerformance from './SecondPerformance';
 import ThirdPerformance from './ThirdPerformance';
 
-const Performance = () => {
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    vertical: true, // Configure slider to work vertically
-    prevArrow: <PrevArrow />,
-    nextArrow: <NextArrow />,
-  };
+// ------------------------------
+// Imports
+// ------------------------------
+// This section has all necessary additional components with animations
 
+function Performance() {
   return (
     <>
-      <Slider {...settings}>
-        <div>
-          <FirstPerformance />
-        </div>
-        <div>
-          <SecondPerformance />
-        </div>
-        <div>
-          <ThirdPerformance />
-        </div>
-      </Slider>
+      <FirstPerformance />
+      <SecondPerformance />
+      <ThirdPerformance />
     </>
   );
-};
-
-// Custom arrow components
-const PrevArrow = (props) => {
-  const { className, onClick } = props;
-  return (
-    <div className={className} onClick={onClick}>
-      Previous
-    </div>
-  );
-};
-
-const NextArrow = (props) => {
-  const { className, onClick } = props;
-  return (
-    <div className={className} onClick={onClick}>
-      Next
-    </div>
-  );
-};
+}
 
 export default Performance;
