de0912e0ef9f960236133c769955058dfbb5890d
gtz.jesus <gtz.jesus@outlook.com>
2024-04-04 12:29:05 -0600

    Performance component adjustments in UI

--- src/components/performance/Performance.jsx
+++ src/components/performance/Performance.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled, { createGlobalStyle } from 'styled-components';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
@@ -13,25 +14,25 @@ const Performance = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    swipeToSlide: true, // Enable swipe gesture
-    swipe: true, // Enable swipe gesture
-    touchMove: true, // Enable touch move
+    vertical: true, // Configure slider to work vertically
     prevArrow: <PrevArrow />,
     nextArrow: <NextArrow />,
   };
 
   return (
-    <Slider {...settings}>
-      <div>
-        <FirstPerformance />
-      </div>
-      <div>
-        <SecondPerformance />
-      </div>
-      <div>
-        <ThirdPerformance />
-      </div>
-    </Slider>
+    <>
+      <Slider {...settings}>
+        <div>
+          <FirstPerformance />
+        </div>
+        <div>
+          <SecondPerformance />
+        </div>
+        <div>
+          <ThirdPerformance />
+        </div>
+      </Slider>
+    </>
   );
 };
 
