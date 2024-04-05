a8c7107d6626ccbf6e729804cba0e3bea09062d5
gtz.jesus <gtz.jesus@outlook.com>
2024-04-04 21:36:01 -0600

    Testing out gifs

--- src/components/performance/sub-performances/GifResponsive.jsx
+++ src/components/performance/sub-performances/GifResponsive.jsx
@@ -6,14 +6,16 @@ const StyledGifResponsive = styled.div`
   background-size: contain;
   background-position: center;
   background-repeat: no-repeat;
-  min-height: 50vh;
+  min-height: 35vh;
 
   /* Apply CSS filter to make the background black and white */
-  filter: grayscale(100%);
+  filter: contrast(250%);
 
   /* Add additional styles to control the zoom effect */
   transition: transform 0.5s ease;
   transform-origin: center;
+
+  margin: var(--margin-large) 0;
 `;
 
 const Title = styled.span`
@@ -21,7 +23,8 @@ const Title = styled.span`
   justify-content: center;
   font-size: var(--font-medium);
   color: var(--color-white);
-  z-index: 999;
+  font-weight: bold; /* Add font weight for emphasis */
+  text-shadow: 2px 2px 2px rgba(0, 0, 0, 0.5); /* Add a subtle text shadow */
 `;
 
 function GifResponsive() {
