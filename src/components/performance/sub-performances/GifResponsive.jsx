ae8fd02a398d34082f5bd63616fa996e5426905f
gtz.jesus <gtz.jesus@outlook.com>
2024-04-06 12:26:08 -0600

    GifResponsive component UI fixes

--- src/components/performance/sub-performances/GifResponsive.jsx
+++ src/components/performance/sub-performances/GifResponsive.jsx
@@ -10,24 +10,12 @@ const StyledGifResponsive = styled.div`
   padding: var(--padding-xxlarge) var(--padding-small);
 
   /* Apply CSS filter to make the background black and white */
-  filter: saturate(1250%);
-`;
-
-const Title = styled.span`
-  display: flex;
-  position: absolute;
-  top: 0;
-  justify-content: left;
-  font-size: var(--font-large);
-  color: var(--color-white);
-  font-weight: bold; /* Add font weight for emphasis */
-  text-shadow: 2px 2px 2px rgba(0, 0, 0, 0.5); /* Add a subtle text shadow */
-  text-transform: uppercase;
+  /* filter: saturate(1250%); */
+  /* filter: invert(100%); */
+  filter: hue-rotate(90deg);
 `;
 
 const SubTitle = styled.span`
-  position: absolute;
-  top: 50;
   display: flex;
   justify-content: left;
   font-size: var(--font-small);
@@ -40,10 +28,7 @@ function GifResponsive() {
   return (
     <LazyLoad>
       <StyledGifResponsive>
-        <Title>
-          <SubTitle>A web</SubTitle> Accessible
-        </Title>
-        <SubTitle>across all devices.</SubTitle>z
+        <SubTitle>Across all devices.</SubTitle>
       </StyledGifResponsive>
     </LazyLoad>
   );
