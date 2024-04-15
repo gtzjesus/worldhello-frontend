83150b637e24b3edcaf9514c4d99f70bbcfa0a01
gtz.jesus <gtz.jesus@outlook.com>
2024-04-15 15:44:07 -0600

    Figma designs

--- src/components/webGif/WebGif.jsx
+++ src/components/webGif/WebGif.jsx
@@ -4,13 +4,11 @@ import LazyLoad from 'react-lazyload';
 const StyledWebGif = styled.div`
   background: url('backgrounds/gifresponsive.gif');
   background-size: contain;
-  background-position: center;
   background-repeat: no-repeat;
-  min-height: 40vh;
+  min-height: 100vh;
   padding: var(--padding-xlarge) var(--padding-small);
 
   /* Apply CSS filter to make the background black and white */
-  filter: invert(100%);
   /* filter: hue-rotate(90deg); */
 `;
 
@@ -33,7 +31,7 @@ function WebGif() {
         <SubTitle>
           What better <br /> way to start <br /> other than
         </SubTitle>
-        <SpiderTitle>With a</SpiderTitle>
+        <SpiderTitle>with a</SpiderTitle>
       </StyledWebGif>
     </LazyLoad>
   );
