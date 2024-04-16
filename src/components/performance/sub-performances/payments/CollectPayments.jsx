9d306b1be01b03325e8ff544426848a6de598f33
gtz.jesus <gtz.jesus@outlook.com>
2024-04-16 11:33:25 -0600

    Navigation fixed

--- src/components/performance/sub-performances/payments/CollectPayments.jsx
+++ src/components/performance/sub-performances/payments/CollectPayments.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useRef } from 'react';
 import styled from 'styled-components';
 
 const StyledCollectPayments = styled.div`
@@ -28,67 +27,16 @@ const Description = styled.span`
 `;
 
 function CollectPayments() {
-  const firstPerformanceRef = useRef(null);
-  const secondPerformanceRef = useRef(null);
-  const thirdPerformanceRef = useRef(null);
-
-  // ------------------------------
-  // useEffect
-  // ------------------------------
-  // Code logic Animation for the whole app, a useEffect so that it happens once component mounts
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      (entries) => {
-        entries.forEach((entry) => {
-          if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-          } else {
-            entry.target.classList.remove('show');
-          }
-        });
-      },
-      { threshold: 0.5 }
-    );
-
-    // Observing all elements of interest
-    const elementsToObserve = [
-      firstPerformanceRef,
-      secondPerformanceRef,
-      thirdPerformanceRef,
-    ];
-    elementsToObserve.forEach((ref) => {
-      if (ref.current) {
-        observer.observe(ref.current);
-      }
-    });
-
-    // Cleanup function
-    return () => {
-      elementsToObserve.forEach((ref) => {
-        if (ref.current) {
-          observer.unobserve(ref.current);
-        }
-      });
-    };
-  }, []);
-
   return (
     <StyledCollectPayments>
-      <div ref={firstPerformanceRef} className="hidden">
-        <Img src="/backgrounds/baggy.webp" alt="signin"></Img>
-      </div>
+      <Img src="/backgrounds/baggy.webp" alt="signin"></Img>
       <Information>
         <Intro>
-          <div ref={secondPerformanceRef} className="hidden">
-            Collect <br /> Payments.
-          </div>
+          Collect <br /> Payments.
         </Intro>
         <br />
         <Description>
-          <div ref={thirdPerformanceRef} className="hidden">
-            Monetize your products or services with a revenue-generating
-            platform.
-          </div>
+          Monetize your products or services with a revenue-generating platform.
         </Description>
       </Information>
     </StyledCollectPayments>
