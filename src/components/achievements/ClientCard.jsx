10e1c435ed70668801ead217e15bcfb4e5fd5cdd
gtz.jesus <gtz.jesus@outlook.com>
2024-06-06 14:41:50 -0600

    frontend ui changes + working backend changes

--- src/components/achievements/ClientCard.jsx
+++ src/components/achievements/ClientCard.jsx
@@ -1,15 +1,25 @@
 import styled from 'styled-components';
 
 const CardContainer = styled.div`
-  /* Styles */
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  /* border-radius: 10px; */
+  margin: 0 var(--margin-xsmall);
+  overflow: hidden;
+
+  &:hover {
+    transform: translateY(-5px);
+  }
 `;
 
 const ClientName = styled.h2`
-  /* Styles */
+  color: #333;
 `;
 
 const ClientImg = styled.img`
-  /* Styles */
+  width: 100%;
 `;
 
 function ClientCard({ client }) {
@@ -17,7 +27,6 @@ function ClientCard({ client }) {
     <CardContainer>
       <ClientName>{client.name}</ClientName>
       <ClientImg src={client.image} alt={client.name} />
-      {/* Add more client information as needed */}
     </CardContainer>
   );
 }
